96d62aa050ade63a52e2e361c7baeb4e77055b80
Emre ÇOĞALAN <comengcogalan@gmail.com>
2022-10-09 13:43:28 +0300

    a
    a

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -23,7 +20,7 @@ public class Main {
                 if ((char) byteData == '\n' || (char) byteData == '\r')
                     break;
             }
-//            socket.getOutputStream().write(0x00b);
+            socket.getOutputStream().write(0x00b);
             System.out.println(hexData);
             socket.close();
         }
