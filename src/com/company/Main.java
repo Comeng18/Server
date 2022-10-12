bc06207a19fe8869f431acc691b85a81fc8058ef
Emre ÇOĞALAN <comengcogalan@gmail.com>
2022-10-12 13:05:47 +0300

    a
    a

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         StringBuilder hexDataBuilder = new StringBuilder();
+        StringBuilder byteDataBuilder = new StringBuilder();
         try {
             // Connect to the server
             ServerSocket server = new ServerSocket(10001);
@@ -15,17 +16,24 @@ public class Main {
                 Socket socket = server.accept();
                 System.out.println("Client Connected");
                 hexDataBuilder = new StringBuilder();
-                byte[] bytes = socket.getInputStream().readAllBytes();
-                for (byte aByte : bytes)
-                    hexDataBuilder.append(String.format("%02X", aByte));
+                while (true) {
+                    int byteData = socket.getInputStream().read();
+                    System.out.println(byteData + " " + String.format("%02X", byteData));
+                    byteDataBuilder.append(byteData);
+                    hexDataBuilder.append(String.format("%02X", byteData));
+//                    if (byteData < 0 || (char) byteData == '\n' || (char) byteData == '\r')
+//                        break;
+                }
 
 //                socket.getOutputStream().write(0x01);
-                System.out.println("Hex Data: " + hexDataBuilder);
-                socket.close();
+//                System.out.println("Hex Data: " + hexDataBuilder);
+//                System.out.println("Hex Data: " + byteDataBuilder);
+//                socket.close();
             }
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Hex Data: " + hexDataBuilder);
+            System.out.println("Hex Data: " + byteDataBuilder);
         }
     }
 }
